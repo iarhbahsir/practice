d4d157bf532cf93cc11f5dc2e8d49a7a6597c3ee
Rishabh Rai <rrai.squared@utexas.edu>
2017-09-23 13:53:15 -0500

    Fixed

--- machine learning/neural_network.py
+++ machine learning/neural_network.py
@@ -18,7 +18,7 @@ class ANN(object):
 
     def __init__(self, layer_sizes, init_biases=None, init_weights=None):
         # create layers of nn
-        self.layer_sizes = np.asarray(list(layer_sizes))
+        self.layer_sizes = np.array(layer_sizes)
 
         if(init_biases != None and init_weights != None):
             self.biases = init_biases
@@ -33,27 +33,19 @@ class ANN(object):
         # uses mini-batch stochastic gradient descent
         print "Started training neural network..."
         # outer for loop to keep track of epochs trained
-        #nn_info = open("nn_info", 'a')
-        #nn_info_alt = open("nn_info_alt", 'w')
         for curr_epoch in xrange(epochs_to_train):
             # shuffle data for better performance
             shuffled_training_data = np.array(training_data)
             random.shuffle(shuffled_training_data)
 
             # inner loop for training all mini_batches in randomized data
-            mb = 0
             for curr_mini_batch in [shuffled_training_data[x: x + mini_batch_size] for x in xrange(0, len(shuffled_training_data), mini_batch_size)]:
-                print "Processing epoch " + str(curr_epoch) + ", mini-batch: " + str(mb) + "/" + str(len(shuffled_training_data)/mini_batch_size) + "\r"
                 self.train_mini_batch(learning_rate, curr_mini_batch)
-                if(mb % 100 == 0):
-                    #nn_info_alt.writelines("Mini-batch" + str(mb) + "\n\n Biases:\n" + str(self.biases) + "\n\nWeights:\n" + str(self.weights))
-                    np.save("nn_biases.npy", self.biases)
-                    np.save("nn_weights.npy", self.weights)
-                mb = mb + 1
+
+            np.save("nn_biases.npy", self.biases)
+            np.save("nn_weights.npy", self.weights)
 
             print "Finished epoch " + str(curr_epoch) + "/" + str(epochs_to_train)
-
-            #nn_info.writelines("Epoch " + str(curr_epoch) + ":\n\n Biases:\n" + str(self.weights) + "\n\nWeights:\n" + str(self.biases))
 
             # periodically evaluate accuracy of nn if desired using test data if given
             if(test_data != None and periodically_evaluate):
@@ -62,81 +54,95 @@ class ANN(object):
                     percent_accurate = self.evaluate_accuracy(test_data)
                     print "Current accuracy is " + str(percent_accurate) + "% (" + str(percent_accurate/100.0 * len(training_data)) + "/" + str(len(training_data)) + ")"
 
-        #nn_info.close()
 
     def train_mini_batch(self, learning_rate, mini_batch):
         # create matrix to store sum of bias and weight cost gradient components
-        del_bias_sum = np.zeros(np.shape(self.biases))
-        del_weight_sum = np.zeros(np.shape(self.weights))
+        del_bias_sum = [np.zeros(np.shape(b)) for b in self.biases]
+        del_weight_sum = [np.zeros(np.shape(w)) for w in self.weights]
 
         for set in mini_batch:
             # use backpropagation to get matrices of weight and bias cost gradient components for the neurons
             del_bias, del_weight = self.backpropagate(set[0], set[1])
 
             # add to total gradients for bias and weight
-            del_bias_sum = np.add(del_bias_sum, del_bias)
-            del_weight_sum = np.add(del_weight_sum, del_weight)
+            #del_bias_sum = np.add(del_bias_sum, del_bias)
+            #del_weight_sum = np.add(del_weight_sum, del_weight)
 
-            # update the weights and the biases simultaneously
-            self.biases = np.subtract(self.biases, np.multiply(del_bias_sum, learning_rate / len(mini_batch)))
+            del_bias_sum = [nb+dnb for nb, dnb in zip(del_bias, del_bias_sum)]
+
+            del_weight_sum = [nw+dnw for nw, dnw in zip(del_weight, del_weight_sum)]
+
+        # update the weights and the biases simultaneously
+        scaled_lr = 1.0 * learning_rate / len(mini_batch)
+        #self.biases = [b - np.multiply(dbs, scaled_lr) for b, dbs in zip(self.biases, del_bias_sum)]
+        self.biases = [b-(learning_rate/len(mini_batch))*nb for b, nb in zip(self.biases, del_bias_sum)]
+        self.weights = [w - (learning_rate / len(mini_batch)) * nw for w, nw in zip(self.weights, del_weight_sum)]
+        #self.weights = [w - np.multiply(dws, scaled_lr) for w, dws in zip(self.weights, del_weight_sum)]
 
     def backpropagate(self, x_in, y_in):
         # calculate activation matrix
         weighted_inputs, activations = self.feedforward(x_in)
 
         # compute output layer error
-        errors = np.array(self.biases)
-        for x in xrange(len(errors)):
-            errors[x] = np.zeros(np.shape(errors[x]))
-        errors[-1] = np.multiply(self.del_activation_cost(activations[-1], y_in), self.sigmoid_derivative(weighted_inputs[-1]))
+        #errors = [np.zeros(np.shape(b)) for b in self.biases]
+        #errors[-1] = self.del_activation_cost(activations[-1], y_in) * self.sigmoid_derivative(weighted_inputs[-1])
+
+        bias_errors = [np.zeros(np.shape(b)) for b in self.biases]
+        weight_errors = [np.zeros(np.shape(w)) for w in self.weights]
+
+        error = self.del_activation_cost(activations[-1], y_in) * self.sigmoid_derivative(weighted_inputs[-1])
+        bias_errors[-1] = error
+        weight_errors[-1] = np.dot(error, np.transpose(activations[-2]))
 
         # backpropagate error, finding error for all layers
-        for layer_index in xrange(len(activations) - 2, 0, -1):
-            errors[layer_index - 1] = np.multiply(np.dot(np.transpose(self.weights[layer_index]), errors[layer_index]), self.sigmoid_derivative(weighted_inputs[layer_index]))
+        #for layer_index in xrange(len(errors) - 2, 0, -1):
+        #    errors[layer_index] = (np.dot(np.transpose(self.weights[layer_index + 1]), errors[layer_index + 1]) *
+        #                            self.sigmoid_derivative(weighted_inputs[layer_index]))
+
+
+        for layer_index in xrange(2, len(self.layer_sizes)):
+            weighted_input = weighted_inputs[-layer_index]
+            sigmoid_weighted_input = self.sigmoid_derivative(weighted_input)
+            error = np.dot(np.transpose(self.weights[-layer_index + 1]), error) * sigmoid_weighted_input
+            bias_errors[-layer_index] = error
+            weight_errors[-layer_index] = np.dot(error, np.transpose(activations[-layer_index - 1]))
 
         # compute cost gradient matrices using activation and error matrices
-        del_weight = np.array(self.weights)
-        """
-        for layer_index in xrange(len(del_weight)):
-            for neuron in xrange(len(del_weight[layer_index])):
-                del_weight[layer_index][neuron] = np.zeros(np.shape(del_weight[layer_index][neuron]))
-        """
-        """
-        for layer_index in xrange(len(self.weights)):
-            for dest_neuron in xrange(len(self.weights[layer_index])):
-                for source_neuron in xrange(len(self.weights[layer_index][dest_neuron])):
-                    del_weight[layer_index][dest_neuron][source_neuron] = activations[layer_index][source_neuron] * errors[layer_index][dest_neuron]
-        """
-        for layer_index in xrange(len(self.weights)):
-            del_weight[layer_index] = np.dot(activations[layer_index + 1], np.transpose(errors[layer_index]))
-        return errors, del_weight
+        #del_weight = [np.zeros(np.shape(dw)) for dw in self.weights]
+
+        #for layer_index in xrange(len(self.weights)):
+        #    del_weight[layer_index] = np.dot(errors[layer_index], np.transpose(activations[layer_index]))
+        return bias_errors, weight_errors
 
     def feedforward(self, inputs):
         # set input layer
-        activations = [np.zeros((neurons, 1)) for neurons in self.layer_sizes]
-        weighted_inputs = np.array((activations))
-        weighted_inputs[0] = np.array(inputs)
-        activations[0] = np.array(inputs)
+        #activations = [np.zeros(neurons, 1) for neurons in self.layer_sizes]
+        #activations[0] = np.array(inputs)
+        #weighted_inputs = [np.zeros(np.shape(b)) for b in self.biases]
+
+        activations = [inputs]
+        activation = inputs
+        weighted_inputs = []
 
         # compute activations
-        for layer_index in xrange(1, len(weighted_inputs)):
-            weighted_inputs[layer_index] = np.dot(self.weights[layer_index - 1], activations[layer_index - 1]) + self.biases[layer_index - 1]
-            activations[layer_index] = self.sigmoid(weighted_inputs[layer_index])
-        """
-        for layer_index in xrange(1, len(weighted_inputs)):
-            for dest_neuron in xrange(len(weighted_inputs[layer_index])):
-                for source_neuron in xrange(len(weighted_inputs[layer_index - 1][dest_neuron])):
-                    weighted_inputs[layer_index][dest_neuron] += self.weights[layer_index - 1][dest_neuron][source_neuron] * weighted_inputs[layer_index - 1][source_neuron]
-                    weighted_inputs[layer_index][dest_neuron] += self.biases[layer_index - 1][dest_neuron]
-            activations[layer_index] = self.sigmoid(weighted_inputs[layer_index])
-        """
+        #for layer_index in xrange(len(weighted_inputs)):
+        #    weighted_inputs[layer_index] = np.dot(self.weights[layer_index], activations[layer_index]) + self.biases[layer_index]
+        #    activations[layer_index + 1] = self.sigmoid(weighted_inputs[layer_index])
+
+        for bias, weight in zip(self.biases, self.weights):
+            weighted_input = np.dot(weight, activation) + bias
+            weighted_inputs.append(weighted_input)
+            activation = self.sigmoid(weighted_input);
+            activations.append(activation)
+
         return weighted_inputs, activations
 
-    def sigmoid(self, to_sigmoid):
-        sigmoided = np.array(np.shape(to_sigmoid))
-        #for neuron in xrange(len(to_sigmoid)):
-        #    sigmoided[neuron] = 1 / (1 + np.exp(-1 * to_sigmoid[neuron]))
+    def feedforward_out(self, inputs):
+        for b, w in zip(self.biases, self.weights):
+            inputs = self.sigmoid(np.dot(w, inputs) + b)
+        return inputs
 
+    def sigmoid(self, to_sigmoid):
         sigmoided = 1 / (1 + np.exp(-1 * to_sigmoid))
 
         return sigmoided
@@ -150,10 +156,10 @@ class ANN(object):
     def evaluate_accuracy(self, test_data):
         num_correct = 0.0
         for set in test_data:
-            activation = self.feedforward(set[0])[1][-1]
-            print "Predicted " + str(np.argmax(activation))
-            print "Actual " + str(set[1])
-            print
+            activation = self.feedforward_out(set[0])
+            #print "Predicted " + str(np.argmax(activation))
+            #print "Actual " + str(set[1])
+            #print
             if(np.argmax(activation) == set[1]):
                 num_correct += 1
 
